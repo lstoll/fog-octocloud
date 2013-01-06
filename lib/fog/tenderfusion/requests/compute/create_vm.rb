1ca58cf9cf03f29c80b3d753c55bcf36a8d608cd
Lincoln Stoll <lstoll@me.com>
2013-01-06 20:58:51 +0100

    Correctly reset vmx config on create.

--- lib/fog/tenderfusion/requests/compute/create_vm.rb
+++ lib/fog/tenderfusion/requests/compute/create_vm.rb
@@ -16,9 +16,29 @@ module Fog
           # Copy all VMDK's over
           Pathname.glob(source.join('*.vmdk')) do |f|
             FileUtils.cp f.expand_path, target
+          end
 
           # TODO: Need to reset MAC/ID/name etc!!
+          setup_uuid_mac(vmname)
+          true
+        end
+
+        private
+
+        def setup_uuid_mac(name)
+          vmx = vmx_for_vm(name)
+          VMXFile.with_vmx_data(vmx) do |data|
+            data.delete "ethernet0.addressType"
+            data.delete "uuid.location"
+            data.delete "uuid.bios"
+            data.delete "ethernet0.generatedAddress"
+            data.delete "ethernet1.generatedAddress"
+            data.delete "ethernet0.generatedAddressOffset"
+            data.delete "ethernet1.generatedAddressOffset"
+            data.delete 'displayname'
+            data['displayName'] = "tenderloin-" + name
           end
+
 
         end
 
