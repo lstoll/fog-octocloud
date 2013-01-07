74e95b19606571ace3e0e76e1af87c46a1dbd9af
Lincoln Stoll <lstoll@me.com>
2013-01-07 17:08:26 +0100

    Handle VM creation & ready check better.

--- lib/fog/octocloud/models/compute/server.rb
+++ lib/fog/octocloud/models/compute/server.rb
@@ -24,12 +24,25 @@ module Fog
           running
         end
 
-        def initialize(attributes={})
-          super
+        def ready?
+          reload
+          running && ip
         end
+
+        def public_ip_address
+          ip
+        end
+
+
+        # def initialize(attributes={})
+          # super
+        # end
 
         def save
           requires :memory, :type, :cube
+
+          attributes[:cube] = cube.kind_of?(Cube) ? cube.name : cube
+          attributes[:memory] = memory.to_i
 
           data = connection.create_vm(attributes)
 
