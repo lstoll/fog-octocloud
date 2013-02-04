141a3503e6d7fcf98ef9f8546b1b9b9281be6b06
Lincoln Stoll <lstoll@me.com>
2013-02-04 18:00:13 +1100

    Split requests spec.

--- spec/remote_vm_requests_spec.rb
+++ spec/remote_vm_requests_spec.rb
@@ -3,36 +3,6 @@ require "helper"
 # TODO - Global vars are so bad. Do this better.
 
 describe "VMRequests" do
-  it "creates a VM"
-
-  it "lists available vms" do
-    res = get_compute.list_vms
-    res.should_not be_empty
-  end
-
-  it "loads VM info" do
-    res = get_compute.get_vm(vm_path)
-    res.should_not be_empty
-    res.should be_kind_of Hash
-  end
-
-  it "starts the VM" do
-    get_compute.start_vm(vm_path)
-    get_compute.get_vm(vm_path)['vm']['running'].should be_true
-  end
-
-  it "gets the IP" do
-    get_compute.get_vm(vm_path)['vm']['ip'].should_not be_nil
-  end
-
-  it "stops the VM" do
-    get_compute.stop_vm(vm_path)
-    get_compute.get_vm(vm_path)['vm']['running'].should be_false
-  end
-
-  it "destroys the VM" do
-    lambda { get_compute.destroy_vm(vm_path) }.should_not raise_error
-  end
 
   describe "CreateRequest" do
     it "should raise without valid options" do
