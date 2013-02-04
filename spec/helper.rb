e3bc91a5ef38956eb226df36badbea8682fa44fe
Lincoln Stoll <lstoll@me.com>
2013-02-04 18:22:38 +1100

    Helper can create local or remote instances.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,9 +1,16 @@
 require 'fog-octocloud'
 
 FOG_MOCK = ENV['FOG_MOCK'] || false
-OCTOCLOUD_API_KEY = ENV['OCTOCLOUD_API_KEY'] || raise("OCTOCLOUD_API_KEY must be set")
-OCTOCLOUD_URL = ENV['OCTOCLOUD_URL'] || raise("OCTOCLOUD_URL must be set")
 
-def get_compute
-  Fog::Compute.new(:provider => "Octocloud", :octocloud_url => OCTOCLOUD_URL, :octocloud_api_key => OCTOCLOUD_API_KEY)
+def get_compute(type)
+  case type
+  when :remote
+    api_key = ENV['OCTOCLOUD_API_KEY'] || raise("OCTOCLOUD_API_KEY must be set for remote compute")
+    url = ENV['OCTOCLOUD_URL'] || raise("OCTOCLOUD_URL must be set for remote compute")
+    Fog::Compute.new(:provider => "Octocloud", :octocloud_url => url, :octocloud_api_key => api_key)
+  when :local
+    Fog::Compute.new(:provider => 'octocloud')
+  else
+    raise ArgumentError.new("Invalid compute type requested")
+  end
 end
