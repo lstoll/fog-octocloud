4fe3352b5db5279864b383a34790aae7b4f2b23c
Lincoln Stoll <lstoll@me.com>
2013-02-04 22:45:46 +1100

    Some code to eventually support optional (gui) starting.

--- lib/fog/octocloud/requests/compute/local_start_vm.rb
+++ lib/fog/octocloud/requests/compute/local_start_vm.rb
@@ -3,10 +3,18 @@ module Fog
     class Octocloud
       class Real
 
-        def local_start_vm(name, opts={:headless => true})
+        # is_vmware_running ? false : true is optimal, but won't delete.
+        def local_start_vm(name, opts={ :headless => true })
           vmx = vmx_for_vm(name)
           gui_opt = opts[:headless] == true ? "nogui" : "gui"
           vmrun('start', :opts => gui_opt, :vmx => vmx)
+        end
+
+        private
+
+        def is_vmware_running
+          `ps aux | grep VMware`
+          $? == 0
         end
 
       end
