4c7b90fa1fb38127a0d3313d3aa4daa061de153a
Lincoln Stoll <lstoll@me.com>
2013-03-02 19:09:19 +1100

    Support uploading .box and .ova as well

--- lib/fog/octocloud/requests/compute/remote_upload_cube.rb
+++ lib/fog/octocloud/requests/compute/remote_upload_cube.rb
@@ -1,10 +1,21 @@
+require 'fog/octocloud/requests/compute/convert_cube'
+
 module Fog
   module Compute
     class Octocloud
       class Real
 
         def remote_upload_cube(id, file)
-          remote_request(:method => :POST, :expects => [200], :path => "/api/cubes/#{id}.vmdk", :body => file )
+          # Get some VMDK action.
+          upload = Pathname.new(file)
+          dir = nil
+          if upload.extname != '.vmdk'
+            dir = Dir.mktmpdir
+            import_file(Pathname.new(dir), upload)
+            upload = Dir[File.join(dir, '*.vmdk')].first
+          end
+          remote_request(:method => :POST, :expects => [200], :path => "/api/cubes/#{id}.vmdk", :body => File.new(upload) )
+          FileUtils.rm_rf(dir) if dir
         end
 
       end
