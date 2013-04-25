f7a21986a18493f6f747fc8af6a0a2112639be51
Lincoln Stoll <lstoll@lstoll.net>
2013-04-25 19:44:26 +0200

    Move RecordingRunner to helper,

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,15 @@
 require 'fog-octocloud'
+
+## Replacement command runner, can be used to see what was executed
+class RecordingRunner
+  @@commands = []
+
+  def self.run(cmd, args = {})
+    # args[:vmx] = args[:vmx].to_s if args[:vmx].kind_of? Pathname
+    @@commands << [cmd, args]
+  end
+
+  def self.commands
+    @@commands
+  end
+end
--- test/unit/local_snapshot_requests_test.rb
+++ test/unit/local_snapshot_requests_test.rb
@@ -41,16 +41,3 @@ class LocalSnapshotMockRequestsTest < MiniTest::Unit::TestCase
     assert @compute.local_snapshot('name', 'snapname')
   end
 end
-
-class RecordingRunner
-  @@commands = []
-
-  def self.run(cmd, args = {})
-    # args[:vmx] = args[:vmx].to_s if args[:vmx].kind_of? Pathname
-    @@commands << [cmd, args]
-  end
-
-  def self.commands
-    @@commands
-  end
-end
