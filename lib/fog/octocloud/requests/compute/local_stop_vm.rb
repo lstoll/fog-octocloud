739ef3b87dcfaead288885aabe15563146f61471
Lincoln Stoll <lstoll@lstoll.net>
2013-04-26 14:51:51 +0200

    If VM fails to soft stop, force stop it

--- lib/fog/octocloud/requests/compute/local_stop_vm.rb
+++ lib/fog/octocloud/requests/compute/local_stop_vm.rb
@@ -6,7 +6,14 @@ module Fog
         def local_stop_vm(name, opts={})
           vmx = vmx_for_vm(name)
           hard_opt = opts[:force] == true ? "hard" : "soft"
-          vmrun 'stop', :opts => hard_opt, :vmx => vmx
+          begin
+            Timeout::timeout(30) {
+              vmrun 'stop', :opts => hard_opt, :vmx => vmx
+            }
+          rescue Timeout::Error
+            # if being nice fails, force it.
+            local_stop_vm(name, :force => true)
+          end
         end
 
       end
