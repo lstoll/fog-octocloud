f484d8a57af25a68701275444ff3981c7154278a
Lincoln Stoll <lstoll@lstoll.net>
2013-04-26 14:52:07 +0200

    check process output for 'running' properly

--- lib/fog/octocloud/requests/compute/local_vm_running.rb
+++ lib/fog/octocloud/requests/compute/local_vm_running.rb
@@ -5,8 +5,8 @@ module Fog
 
         def local_vm_running(name)
           vmx = vmx_for_vm(name)
-          vmrun "list | grep '#{vmx}'"
-          $? == 0 ? true : false
+          running = vmrun "list"
+          running.include? vmx.to_s
         end
 
       end
